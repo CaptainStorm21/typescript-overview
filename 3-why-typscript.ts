8e24ccd8bc52f1e18ed3d6d9c4bc71f21d068171
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-07-07 23:29:00 -0700

    update on a build and lessons 3 and 4

---
+++ 3-why-typscript.ts
@@ -0,0 +1,10 @@
+// Why use Typescript
+
+// Typescript knows about the different properties and methods that every type has
+
+// if we refer to properties or methods that do not exist on a value,
+// it will display an error 
+
+const sentence: string = 'To be, or not to be, that is the question.';
+console.log(sentence.includes('is'));
+
---
+++ 4-ts-with-functions.ts
@@ -0,0 +1,12 @@
+// Typescript with Functions
+
+const add = (a: number, b: number) : number => {
+  return a + b;
+}
+
+console.log('Output is ' + add(5, 3));
+
+const joinStrings = (a: string, b: string): string => {
+  return a + b;
+};
+console.log('Alex ' + 'Bobek');
